fbe7b7fedc4a645023228ed838f31cf633a5e934
Dawid Kołba <kolba.dawid@gmail.com>
2024-01-23 20:50:14 +0100

    initial commit, work in progress

---
+++ FilesUnblockerForWindows/FilesUnblocker.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.InteropServices;
+
+
+namespace FilesUnblockerForWindows
+{
+    internal class FilesUnblocker
+    {
+        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool DeleteFile(string name);
+        private static readonly NLog.Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+
+        private static async Task<bool> Unblock(string fileName)
+        {
+            return DeleteFile(fileName + ":Zone.Identifier");
+        }
+
+        public static async Task SearchDirectory(string directoryPath)
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(directoryPath);
+                foreach (string file in files)
+                {
+                    await ProcessFile(file);
+                }
+
+                string[] directories = Directory.GetDirectories(directoryPath);
+                foreach (string dir in directories)
+                {
+                    await SearchDirectory(dir);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (Options.CreateLogFile)
+                    _logger.Info(ex, "Exception");
+            }
+        }
+
+        private static async Task ProcessFile(string filePath)
+        {
+            if (Options.LogAlsoAlreadyUnblockedFiles)
+            {
+                if (Options.CreateLogFile)
+                    _logger.Info($"Current processing file: {filePath}");
+            }
+            await Unblock(filePath);
+        }
+    }
+}
---
+++ FilesUnblockerForWindows/Program.cs
@@ -0,0 +1,95 @@
+﻿using FilesUnblockerForWindows;
+using System.Windows.Forms;
+
+SystemHelper.LoadSettings();
+
+
+string txt = Clipboard.GetText(TextDataFormat.UnicodeText).ToString();
+
+bool extraUnblock = false;
+if (Directory.Exists(txt))
+{
+    Console.WriteLine("Detected following path to directory in clipboard: \n" + txt);
+    string odp;
+    if (askIfUnlockDirFromClipboard)
+    {
+        Console.WriteLine("Would you like to unblock files in this directory with recursive scan?(y/n)");
+        odp = Console.ReadLine().ToString();
+    }
+    else
+    {
+        odp = "y";
+    }
+    if (odp.ToLower() == "y")
+    {
+        Console.WriteLine("Extra unblock is active");
+        extraUnblock = true;
+    }
+    else
+    {
+
+    }
+    //Console.WriteLine("wprowadzona odp: " + odp);
+
+}
+
+
+loguj("Application started");
+if (UsingRecursiveScan)
+{
+    loguj("Using recursive scan");
+    searchDirectory();
+    searchDirectory(Directory.GetCurrentDirectory());
+
+    if (extraUnblock)
+    {
+
+        try
+        {
+            Console.WriteLine("extra unblock started");
+            searchDirectory(txt, true);
+        }
+        catch { }
+    }
+    if (argScan)
+    {
+
+        try
+        {
+            Console.WriteLine("unblock files from paths given by cmd parameters started");
+            searchDirectory(args[0], true);
+        }
+        catch { }
+    }
+}
+else
+{
+    loguj("Without recursive scan");
+    searchDirectory();
+    if (extraUnblock)
+    {
+
+        try
+        {
+            Console.WriteLine("extra unblock started2");
+            searchDirectory(txt, true);
+        }
+        catch { }
+        if (argScan)
+        {
+
+            try
+            {
+                Console.WriteLine("unblock files from paths given by cmd parameters started");
+                searchDirectory(args[0], true);
+            }
+            catch { }
+        }
+    }
+}
+loguj("Changed " + counterUnblockFiles + " files. Checked " + counterCheckedFiles + " files. Have a nice day ;)");
+if (!FastMode)
+{
+    Console.WriteLine("Press any key to exit");
+    Console.ReadKey();
+}
